c6a367f096f72952fb7140cd8ab2b3361ae76ab6
Vitaly Kabaev <vitalik-kabaev@mail.ru>
2022-04-03 23:57:24 +0700

    ui(MainForm): добавлена верстка главного окна

--- src/NoteApp/NoteApp.View/MainForm.cs
+++ src/NoteApp/NoteApp.View/MainForm.cs
@@ -16,5 +16,40 @@ namespace NoteApp.View
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
