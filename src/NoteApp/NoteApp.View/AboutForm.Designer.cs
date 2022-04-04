a1122dd4d9fcad356dd24bbf4639b9f227b2cc58
Vitaly Kabaev <vitalik-kabaev@mail.ru>
2022-04-04 19:33:50 +0700

    ui(NoteForm): Закончил основу свех форм

---
+++ src/NoteApp/NoteApp.View/AboutForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace NoteApp.View
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.AuthorLabel = new System.Windows.Forms.Label();
+            this.NameAuthorLabel = new System.Windows.Forms.Label();
+            this.VkontakteLabel = new System.Windows.Forms.Label();
+            this.GitLabel = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(49, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NoteApp";
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VersionLabel.Location = new System.Drawing.Point(52, 93);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(57, 20);
+            this.VersionLabel.TabIndex = 1;
+            this.VersionLabel.Text = "v 1.0.0";
+            // 
+            // AuthorLabel
+            // 
+            this.AuthorLabel.AutoSize = true;
+            this.AuthorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AuthorLabel.Location = new System.Drawing.Point(52, 134);
+            this.AuthorLabel.Name = "AuthorLabel";
+            this.AuthorLabel.Size = new System.Drawing.Size(63, 20);
+            this.AuthorLabel.TabIndex = 2;
+            this.AuthorLabel.Text = "Author:";
+            // 
+            // NameAuthorLabel
+            // 
+            this.NameAuthorLabel.AutoSize = true;
+            this.NameAuthorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameAuthorLabel.Location = new System.Drawing.Point(121, 134);
+            this.NameAuthorLabel.Name = "NameAuthorLabel";
+            this.NameAuthorLabel.Size = new System.Drawing.Size(114, 20);
+            this.NameAuthorLabel.TabIndex = 3;
+            this.NameAuthorLabel.Text = "Kabaev Vitaliy";
+            // 
+            // VkontakteLabel
+            // 
+            this.VkontakteLabel.AutoSize = true;
+            this.VkontakteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VkontakteLabel.Location = new System.Drawing.Point(52, 177);
+            this.VkontakteLabel.Name = "VkontakteLabel";
+            this.VkontakteLabel.Size = new System.Drawing.Size(90, 20);
+            this.VkontakteLabel.TabIndex = 4;
+            this.VkontakteLabel.Text = "VKontakte:";
+            // 
+            // GitLabel
+            // 
+            this.GitLabel.AutoSize = true;
+            this.GitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GitLabel.Location = new System.Drawing.Point(52, 208);
+            this.GitLabel.Name = "GitLabel";
+            this.GitLabel.Size = new System.Drawing.Size(67, 20);
+            this.GitLabel.TabIndex = 5;
+            this.GitLabel.Text = "GitHub:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel1.Location = new System.Drawing.Point(148, 177);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(87, 20);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "kabaev_vit";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel2.Location = new System.Drawing.Point(125, 208);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(64, 20);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "aliestro";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 424);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.GitLabel);
+            this.Controls.Add(this.VkontakteLabel);
+            this.Controls.Add(this.NameAuthorLabel);
+            this.Controls.Add(this.AuthorLabel);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "AboutForm";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.Label AuthorLabel;
+        private System.Windows.Forms.Label NameAuthorLabel;
+        private System.Windows.Forms.Label VkontakteLabel;
+        private System.Windows.Forms.Label GitLabel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
