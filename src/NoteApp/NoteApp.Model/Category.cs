891c4072fdf9ee954b3b63fff740979d77f3f058
Vitaly Kabaev <vitalik-kabaev@mail.ru>
2022-04-18 22:36:24 +0700

    Создал бизнес-логику

---
+++ src/NoteApp/NoteApp.Model/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Model
+{
+
+    public enum Category
+    {
+        Jobe = 0,
+        Home = 1,
+        HealthandSports = 2,
+        People = 3,
+        Documents = 4,
+        Finance = 5,
+        Differnt = 6,
+    }
+
+}
---
+++ src/NoteApp/NoteApp.Model/Note.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NoteApp.Model
+{
+    /// <summary>
+    /// Описывает блокнот.
+    /// </summary>
+    public class Note
+    {
+
+
+        /// <summary>
+        /// Название заметки.
+        /// </summary>
+        private string _title;
+        /// <summary>
+        /// Категория заметки.
+        /// </summary>
+        private Category _category;
+        /// <summary>
+        /// Возвращает или задает текст заголовка.
+        /// </summary>
+        /// 
+        private string _text;
+        /// <summary>
+        /// Время создания заметки.
+        /// </summary>
+        /// 
+        private DateTime _creationTime;
+        /// <summary>
+        /// Время последнего измения заметки.
+        /// </summary>
+        /// 
+        private DateTime _lastModifiedTime;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+            }
+        }
+        /// <summary>
+        /// Возвращает или задает категорию заметки
+        /// </summary>
+        public Category Category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+            }
+        }
+
+        public Note(string title, Category category, string text)
+        {
+            _title = title;
+            _category = category;
+            _text = text;
+        }
+
+        /// <summary> 
+    }
+
+}
